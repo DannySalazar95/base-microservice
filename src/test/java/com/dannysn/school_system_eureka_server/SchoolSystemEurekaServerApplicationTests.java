da1f0df1817303cdfec70f905b04e7a5527a8442
Danny Salazar <danny.namuche.1995@gmail.com>
2021-04-04 18:17:01 -0500

    first commit

---
+++ src/test/java/com/dannysn/school_system_eureka_server/SchoolSystemEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannysn.school_system_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSystemEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
